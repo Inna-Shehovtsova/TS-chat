1dc25efc9eaac062d2861756beca65dc1cdf42c9
Inna-Shehovtsova <innashehovtsova@gmail.com>
2024-08-24 16:45:12 +0300

    fix lint staged

--- src/costAccounting/costAccounting.ts
+++ src/costAccounting/costAccounting.ts
@@ -5,7 +5,9 @@ export interface IConsumption {
   quantity: number;
   comment: string;
 }
+
 export type tCategorizedSum = [string, number];
+
 export interface IConsumptionStatistic {
   categorizedExpenditure(
     cat?: string,
@@ -16,19 +18,23 @@ export interface IConsumptionStatistic {
   filteredSum(filter: (el: IConsumption) => boolean): number;
   sortedCatSum(startDate?: Date, endDate?: Date): tCategorizedSum[];
 }
+
 export interface IConsuptionFunc {
   getCategories(): Set<string>;
   setItem(cat: string, subcat: string, sum: number, comment: string): void;
   removeItem(cat: string, subcat: string, sum: number, date: Date): void;
 }
+
 export class CostAccounting implements IConsumptionStatistic, IConsuptionFunc {
   accounting: Array<IConsumption>;
+
   getCategories(): Set<string> {
     return this.accounting.reduce(
       (acc, el) => acc.add(el.cat),
       new Set<string>(),
     );
   }
+
   setItem(cat: string, subcat: string, sum: number, comment: string): void {
     this.accounting.push({
       cat: cat,
@@ -38,6 +44,7 @@ export class CostAccounting implements IConsumptionStatistic, IConsuptionFunc {
       date: new Date(),
     });
   }
+
   isEqualDay(day1: Date, day2: Date): boolean {
     const ret =
       day1.getFullYear() == day2.getFullYear() &&
@@ -45,6 +52,7 @@ export class CostAccounting implements IConsumptionStatistic, IConsuptionFunc {
       day1.getDate() == day2.getDate();
     return ret;
   }
+
   removeItem(cat: string, subcat: string, sum: number, date: Date): void {
     this.accounting = this.accounting.filter(
       (el) =>
@@ -54,6 +62,7 @@ export class CostAccounting implements IConsumptionStatistic, IConsuptionFunc {
         !this.isEqualDay(el.date, date),
     );
   }
+
   categorizedExpenditure(cat?: string, startDate?: Date, endDate?: Date) {
     return this.accounting
       .filter((el) => {
@@ -71,14 +80,17 @@ export class CostAccounting implements IConsumptionStatistic, IConsuptionFunc {
       })
       .reduce((prev, curr) => prev + curr.quantity, 0);
   }
+
   uncatExpenditure(startDate?: Date, endDate?: Date) {
     return this.categorizedExpenditure(undefined, startDate, endDate);
   }
+
   filteredSum(cbfilter: (el: IConsumption) => boolean): number {
     return this.accounting
       .filter(cbfilter)
       .reduce((prev, curr) => prev + curr.quantity, 0);
   }
+
   sortedCatSum(startDate?: Date, endDate?: Date): tCategorizedSum[] {
     const ret: tCategorizedSum[] = Array<tCategorizedSum>();
     this.getCategories().forEach((el, el2) => ret.push([el, 0]));
@@ -87,6 +99,7 @@ export class CostAccounting implements IConsumptionStatistic, IConsuptionFunc {
     );
     return ret.sort((a, b) => b[1] - a[1]);
   }
+
   constructor(ccounting: Array<IConsumption>) {
     this.accounting = ccounting;
   }
